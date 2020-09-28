e7afe5aa544190583a32ec7ed0d889570c7725cf
Will <doggy.huang@gmail.com>
2020-09-28 10:13:38 +0800

    示範 SynchronizationContext 用法

--- SyncContextDemo/Controllers/HomeController.cs
+++ SyncContextDemo/Controllers/HomeController.cs
@@ -1,30 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SyncContextDemo.Controllers
+namespace SynContextDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task Index()
         {
-            return View();
+            // ASP.NET 預設採用 AspNetSynchronizationContext 實作
+            // https://referencesource.microsoft.com/#System.Web/AspNetSynchronizationContext.cs,9b31a5e94e4b9894
+            var sc = SynchronizationContext.Current;
+
+            Log(1);
+
+            await Task.Run(() =>
+            {
+                Log(2);
+
+                // SynchronizationContext.Post(SendOrPostCallback, Object) 方法
+                // https://docs.microsoft.com/zh-tw/dotnet/api/system.threading.synchronizationcontext.post
+                // Post方法會啟動「非同步要求」以張貼訊息。
+                sc.Post((d) =>
+                {
+                    Thread.Sleep(1000);
+                    // 這裡會使用 sc 當下取得的 Thread Context 來執行以下程式碼
+                    Helpers.Log(3);
+                }, null);
+
+                Log(4);
+            });
+
+            Log(5);
         }
-
-        public ActionResult About()
+        public void Log(int num)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Response.Write($"<h1>{num}</h1>");
         }
+    }
 
-        public ActionResult Contact()
+    public static class Helpers
+    {
+        public static void Log(int num)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            HttpContext.Current.Response.Write($"<h1>{num}</h1>");
         }
     }
 }
